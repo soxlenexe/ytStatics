9ab343fdc2727dab3eaf29b4c5cc8620d24c43ea
soxlenexe <70555800+soxlenexe@users.noreply.github.com>
2022-01-28 13:15:58 -0300

    Update api.js

--- api.js
+++ api.js
@@ -47,6 +47,20 @@ export const isMonetized = async (url) => {
                     views.split(' ')[0];
                 }
 
+                let likes = res().contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.videoActions.menuRenderer.topLevelButtons[0].toggleButtonRenderer.defaultText?.simpleText
+                likes = likes ? likes : 0;
+
+                if(likes != 0){
+                    if(likes.includes(',')){
+                        likes = parseInt(likes.replace(',','')) * 10000;
+
+                    }else{
+                        likes = parseInt(likes.replace('.',''));
+
+                    }
+
+                }
+
                 if (subs != 0) {
 
                     if (subs.includes(',')) {
@@ -68,12 +82,12 @@ export const isMonetized = async (url) => {
                     }
                 }
 
-
+                let monteized = false;
                 if (data.includes('ads') && views >= 4000 && subs >= 1000) {
-                    resolve(true)
-                } else {
-                    resolve(false)
-                }
+                   monteized = true;
+                } 
+                    resolve([monteized,likes])
+                
             });
         })
 
@@ -91,13 +105,69 @@ export const isMonetized = async (url) => {
 
 
 
-export const WEB_LIST = async (KeyWord) => {
+export const VIDEO_LIST = async (KeyWord, filter = null) => {
 
 
     return new Promise((resolve, reject) => {
-        let vid = 'EgIQAQ%253D%253D',
-            can = 'EgIQAg%253D%253D';
-        const request = https.request(`https://www.youtube.com/results?search_query=${KeyWord}&sp=${vid}`, (response) => {
+        let vid = 'EgIQAQ%253D%253D';
+        const request = https.request(`https://www.youtube.com/results?search_query=${KeyWord}&sp=${filter?filter:vid}`, (response) => {
+
+            let data = '';
+            response.on('data', (chunk) => {
+                data = data + chunk.toString();
+            });
+
+            response.on('end', async () => {
+                const body = data;
+
+
+                let parser = new JSDOM(body);
+                let document = parser.window.document;
+                let rawJS = '';
+                Object.values(document.querySelectorAll('script')).map(script => {
+                    if (script.innerHTML.includes('var ytInitialData')) {
+                        rawJS = script.innerHTML;
+                    }
+                });
+                let res = new Function(rawJS + `
+                    return ytInitialData;
+                `);
+
+                let videolist = res().contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents[0].itemSectionRenderer.contents;
+                videolist = Object.values(videolist);
+
+                let finalList = [];
+
+                await Promise.all(videolist.map(async v => {
+                    let vi = v;
+                    if (v.videoRenderer ?.videoId) {
+                        let data= await isMonetized('https://www.youtube.com/watch?v=' + vi.videoRenderer.videoId);
+                        vi.videoRenderer.isMonetized = data[0];
+                        vi.videoRenderer.likes = data[1];
+                        finalList.push(vi);
+                    }
+                }));
+
+                resolve(finalList);
+            });
+        })
+
+        request.on('error', (error) => {
+            reject(new Error(error));
+        });
+
+        request.end()
+
+    });
+}
+
+
+export const CANAL_LIST = async (KeyWord) => {
+
+
+    return new Promise((resolve, reject) => {
+        let can = 'EgIQAg%253D%253D';
+        const request = https.request(`https://www.youtube.com/results?search_query=${KeyWord}&sp=${can}`, (response) => {
 
             let data = '';
             response.on('data', (chunk) => {
@@ -141,197 +211,5 @@ export const WEB_LIST = async (KeyWord) => {
 
 
 
-export const VIDEO_CANAL = async (KeyWord, filter = null) => {
 
-
-    return new Promise((resolve, reject) => {
-        let vid = 'EgIQAQ%253D%253D';
-        const request = https.request(`https://www.youtube.com/results?search_query=${KeyWord}&sp=${filter?filter:vid}`, (response) => {
-
-            let data = '';
-            response.on('data', (chunk) => {
-                data = data + chunk.toString();
-            });
-
-            response.on('end', async () => {
-                const body = data;
-
-
-                let parser = new JSDOM(body);
-                let document = parser.window.document;
-                let rawJS = '';
-                Object.values(document.querySelectorAll('script')).map(script => {
-                    if (script.innerHTML.includes('var ytInitialData')) {
-                        rawJS = script.innerHTML;
-                    }
-                });
-                let res = new Function(rawJS + `
-                    return ytInitialData;
-                `);
-
-                let videolist = res().contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents[0].itemSectionRenderer.contents;
-                videolist = Object.values(videolist);
-
-                let finalList = [];
-
-                await Promise.all(videolist.map(async v => {
-                    let vi = v;
-                    if (v.videoRenderer ?.videoId) {
-
-                        vi.videoRenderer.isMonetized = await isMonetized('https://www.youtube.com/watch?v=' + vi.videoRenderer.videoId);
-                        finalList.push(vi);
-                    }
-                }));
-
-                resolve(finalList);
-            });
-        })
-
-        request.on('error', (error) => {
-            reject(new Error(error));
-        });
-
-        request.end()
-
-    });
-}
-
-
-
-
-// --------------------------------------------------------------------------------------------------------------------------
-export let apiUrl = {
-    baseUrl: 'https://www.googleapis.com/youtube/v3/search?',
-    part: 'snippet',
-    type: 'video',
-    //order: 'viewCount',
-    maxResults: '',
-    q: '',
-    //key: 'AIzaSyAuc9S4fEC3oGMoLJbUQdV0SZ7KIon4vf8',
-    key: 'AIzaSyBdgAYC6bjc0U90msoR15V03nBfvj6MN9o',
-    /*prevPageToken: '',
-    nextPageToken: ''*/
-}
-
-export let apiStatistics = {
-    baseUrlStatistics: 'https://www.googleapis.com/youtube/v3/videos?',
-    partStatistics: 'id,snippet,contentDetails,status,statistics,topicDetails',
-    //partStatistics: 'statistics,snippet,contentDetails',
-    //keyStatistics: 'AIzaSyAuc9S4fEC3oGMoLJbUQdV0SZ7KIon4vf8',
-    keyStatistics: 'AIzaSyBdgAYC6bjc0U90msoR15V03nBfvj6MN9o',
-    maxResultsStatistics: '',
-    //orderStatistics: 'viewCount',
-    id: ''
-}
-
-
-
-
-export const ListVideos = async (q, maxResults) => {
-
-
-
-
-    // apiStatistics.maxResultsStatistics = data.results;
-
-    const {
-        baseUrl,
-        part,
-        type,
-        order,
-
-        key
-    } = apiUrl;
-
-    const apiUrlVideos = `${baseUrl}part=${part}&type=${type}&q=${q}&maxResults=${maxResults}&key=${key}`;
-
-    let res = await axios.get(apiUrlVideos).catch(console.log)
-
-
-    let nextPage = res.data.nextPageToken;
-    let prevPage = res.data.prevPageToken
-
-    let responseVideos = res.data.items;
-    let textConcatVideosIds = '';
-
-    for (var i = 0; i < responseVideos.length; i++) {
-        textConcatVideosIds += responseVideos[i].id.videoId + ",";
-    }
-
-    apiStatistics.id = textConcatVideosIds;
-
-    const {
-        baseUrlStatistics,
-        partStatistics,
-        orderStatistics,
-        id,
-        maxResultsStatistics,
-        keyStatistics
-    } = apiStatistics;
-    const urlStatistics = `${baseUrlStatistics}part=${partStatistics}&id=${id}&maxResults=${maxResults}&key=${keyStatistics}`;
-
-    let rest = await axios.get(urlStatistics).catch(console.log);
-
-    let responseStatistics = rest.data.items;
-    // console.log('videos', responseStatistics)
-    return {
-        responseStatistics,
-        nextPage,
-        prevPage
-    };
-
-
-}
-
-
-
-
-
-
-
-
-export const PrevPage = async (apiUrl) => {
-
-
-
-    const {
-        baseUrl,
-        part,
-        type,
-        order,
-        maxResults,
-        q,
-        key,
-        prevPageToken
-    } = apiUrl;
-
-    const apiUrlVideos = `${baseUrl}part=${part}&type=${type}&q=${q}&maxResults=${maxResults}&key=${key}&pageToken=${prevPageToken}`;
-
-    let res = await axios.get(apiUrlVideos).catch(console.log);
-
-
-    let nextPage = res.data.nextPageToken;
-    let prevPage = res.data.prevPageToken
-
-    let responseVideos = res.data.items;
-    let textConcatVideosIds = '';
-
-    for (var i = 0; i < responseVideos.length; i++) {
-        textConcatVideosIds += responseVideos[i].id.videoId + ",";
-    }
-
-    apiStatistics.id = textConcatVideosIds;
-
-
-    let rest = await axios.get(urlStatistics).catch(console.log);
-
-    let responseStatistics = rest.data.items;
-
-    return {
-        responseStatistics,
-        nextPage,
-        prevPage
-    };
-
-}
 
